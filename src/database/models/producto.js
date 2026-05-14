97500f0cf5888c700899a985ec9143db3cadb02c
Claudia Ilarragorri <cilarragorri@gmail.com>
2026-05-14 00:01:12 -0300

    nueva actualizacion
    ...

---
+++ src/database/models/producto.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+
+  const Producto = sequelize.define('producto', {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    codigo: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      field: 'deleted_at'
+    }
+  }, {
+    paranoid: true,
+    freezeTableName: true
+  });
+
+  Producto.associate = models => {
+    //relaciones
+  }
+
+  return Producto;
+};
